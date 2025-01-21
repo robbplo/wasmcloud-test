a31b7e18fa1657bbeac4424ca45399c7bf20949e
Robbin Ploeger <robbin.ploeger@bettyblocks.com>
2025-01-21 04:58:36 +0000

    cleanup

--- random-number/src/lib.rs
+++ random-number/src/lib.rs
@@ -1,12 +1,14 @@
 wit_bindgen::generate!({ generate_all });
 
 use wasmcloud_component::wasi::random::random;
-use crate::exports::wasmcloud::random_number::random_number::Guest;
+use crate::exports::robbin::random_number::random_number::Guest;
+
+export!(RandomNumber);
 
 struct RandomNumber;
 
 impl Guest for RandomNumber {
-    fn random_int() -> u64 {
+    fn test() -> u64 {
         random::get_random_u64()
     }
 }
